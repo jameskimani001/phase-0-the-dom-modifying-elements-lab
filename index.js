30a5553614526ac6a7f299558b837c2e433d61f3
jameskimani001 <jameskim092t@gmail.com>
2024-10-08 09:25:04 +0300

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+
+let newHeader;
+
+
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+}
+
+
+newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'Techie kim is the champion'; 
+
+
+document.body.appendChild(newHeader);
